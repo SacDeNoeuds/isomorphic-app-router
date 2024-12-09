b4a37b8773b749288ff6bfe72924e0535619e65b
SacDeNoeuds <sacdenoeuds.dev@gmail.com>
2024-12-09 16:24:11 +0100

    add typedoc

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
+/**
+ * @module
+ * @document ../guides/nested-router.md
+ * @document ../guides/route-comparison.md
+ * @document ../guides/enforcing-a-route-shape.md
+ * @document ../guides/router-level-history-override.md
+ * @document ../guides/using-another-history.md
+ * @document ../guides/framework-integration.md
+ */
 export { PathToRegexpResolver } from './resolver/PathToRegexp'
 export { URLPatternResolver } from './resolver/URLPattern'
 export type {
---
+++ typedoc.config.js
@@ -0,0 +1,36 @@
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+
+/** @type {Partial<import('typedoc').TypeDocOptions>} */
+const config = {
+  basePath: isGithubActions ? './unhoax' : './docs',
+  entryPoints: ['./src/main.ts'],
+  plugin: ['typedoc-unhoax-theme'],
+  out: 'docs',
+  includeVersion: true,
+  searchInComments: true,
+  searchInDocuments: true,
+  categorizeByGroup: false,
+  navigation: {
+    compactFolders: true,
+    includeCategories: true,
+    excludeReferences: true,
+    includeGroups: false,
+  },
+  visibilityFilters: {},
+  defaultCategory: 'Default Category',
+  cleanOutputDir: true,
+  excludeCategories: ['Default Category'],
+  categoryOrder: [
+    'Guide',
+    'Parsing',
+    'Schema',
+    'Modifier',
+    'Refinement',
+    'Schema Definition',
+    'Advanced Usage / Core',
+    '*',
+  ],
+  sort: ['documents-first', 'alphabetical'],
+}
+
+export default config
