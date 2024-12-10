efd7dc52c896f522bce624d5ec5569d365121f98
SacDeNoeuds <sacdenoeuds.dev@gmail.com>
2024-12-10 21:36:53 +0100

    use istanbul for code coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   test: {
     coverage: {
       ...(isCI && { reporter: ['json-summary'] }),
+      provider: 'istanbul',
       include: ['src'],
       exclude: [
         '**/*.spec.ts',
